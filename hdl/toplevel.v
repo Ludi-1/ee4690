20b3e21f9fddd82898f7bc84c541ac41d575210a
Jarl Brand <jarl.brand@gmail.com>
2024-05-05 13:48:26 +0200

    toplevel file created

---
+++ hdl/toplevel.v
@@ -0,0 +1,18 @@
+module toplevel #(
+    parameter MATRIX_DIM = 64,
+    parameter INPUT_WIDTH = 8,
+    parameter OUTPUT_WIDTH = 20,
+)
+
+(
+    input CLK,
+    input rst
+);
+
+always @ (posedge CLK) begin
+    
+end
+
+
+
+endmodule
