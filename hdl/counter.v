df9480dbf0684c79033da6150cc20f3def417e2c
Ludi-1 <liou.hu@protonmail.com>
2024-05-05 12:54:42 +0200

    rtl: counters finished

--- hdl/counter.v
+++ hdl/counter.v
@@ -1,6 +1,7 @@
 module counter #(
-    parameter COUNT1_MAX = 8,
-    parameter COUNT2_MAX = 8
+    parameter COUNT_MAX = 8,
+    parameter COUNT1_INC = 1,
+    parameter COUNT2_INC = 8
 )
 
 (
@@ -8,24 +9,27 @@ module counter #(
     input rst,
     input count_en,
 
-    output reg [COUNT1_MAX-1:0] count1,
-    output reg [COUNT2_MAX-1:0] count2
+    output reg [COUNT_MAX:0] count
 );
+
+reg [COUNT_MAX-1:0] count1, count2;
+
+assign count = count1 + count2;
 
 always @(posedge CLK, posedge rst) begin
     if (rst) begin
         count1 <= '0;
         count2 <= '0;
     end else if (count_en) begin
-        if (count1 == COUNT1_MAX-1) begin
+        if (count1 == (COUNT_MAX-1)*COUNT1_INC) begin
             count1 <= 0;
-            if (count2 == COUNT2_MAX-1) begin
+            if (count2 == (COUNT_MAX-1)*COUNT2_INC) begin
                 count2 <= 0;
             end else begin
-                count2 <= count2 + 1;
+                count2 <= count2 + COUNT2_INC;
             end
         end else begin
-            count1 <= count1 + 1;
+            count1 <= count1 + COUNT1_INC;
         end
     end
 end
