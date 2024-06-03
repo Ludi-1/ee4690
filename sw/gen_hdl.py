1d019646ad1ce6ae5fb3596690e5164f6e2b1cd7
Redamancy-520 <65457793+Redamancy-520@users.noreply.github.com>
2024-06-03 17:18:39 +0200

    Update gen_hdl.py

--- sw/gen_hdl.py
+++ sw/gen_hdl.py
@@ -13,90 +13,98 @@ import os
 from sw import templates
 
 # Image dataset
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-train_images = train_images.reshape((60000, 28, 28, 1))
-test_images = test_images.reshape((10000, 28, 28, 1))
-
-train_images, test_images = train_images / 127.5 - 1, test_images / 127.5 - 1
-
-# NN Topology
 kwargs = dict(input_quantizer="ste_sign",
               kernel_quantizer="ste_sign",
               kernel_constraint="weight_clip")
 
 model = tf.keras.models.Sequential()
+
+input_shape = (28, 28, 1) # Input img shape
+filters_a = 32 # Number of output channels
+kernel_three = (4, 4) # Kernel dimension
+
+filters_b = 32 # Number of output channels
+kernel_b = (3, 3) # Kernel dimension
+
+model.add(lq.layers.QuantConv2D(filters_a, kernel_three,
+                                input_quantizer="ste_sign",
+                                kernel_quantizer="ste_sign",
+                                kernel_constraint="weight_clip",
+                                use_bias=False,
+                                input_shape=input_shape))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+model.add(lq.layers.QuantConv2D(filters_b, kernel_b,
+                                input_quantizer="ste_sign",
+                                kernel_quantizer="ste_sign",
+                                kernel_constraint="weight_clip",
+                                use_bias=False,
+                                input_shape=input_shape))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
 model.add(tf.keras.layers.Flatten())
+model.add(lq.layers.QuantDense(128, use_bias=False, **kwargs))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
 model.add(lq.layers.QuantDense(10, use_bias=False, **kwargs))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
 model.add(tf.keras.layers.Activation("softmax"))
+# model.add(tf.keras.layers.Flatten())
+# # model.add(lq.layers.QuantDense(500, use_bias=False, **kwargs))
+# model.add(lq.layers.QuantDense(10, use_bias=False, **kwargs))
+# model.add(tf.keras.layers.Activation("softmax"))
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 model.fit(train_images, train_labels, batch_size=64, epochs=6)
+
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 lq.models.summary(model)
 
 if not os.path.exists("gen_hdl"):
     os.mkdir("gen_hdl")
+  
+for layer in model.layers:
+    if isinstance(layer, tf.keras.layers.BatchNormalization):
+        beta, moving_mean, moving_variance = layer.get_weights()
+        print(f"Layer: {layer.name}")
+        print(f"  Beta (offset): {beta}")
+        print(f"Beta Length: {len(beta)}")
+        print(f"  Moving Mean: {moving_mean}")
+        print(f" Moving Mean Length: {len(moving_mean)}")
+        print(f"  Moving Variance: {moving_variance}")
+        print(f"  Moving Variance Length: {len(moving_variance)}")
+
 
 ### PARSE FC FUNC
-def parse_fc(fc_weights, num: int, classifier: bool):
-    if classifier:
-        classifier = str(1)
-    else:
-        classifier = str(0)
+betas = []
+moving_means = []
+moving_variances = []
+for layer in model.layers:
+    if isinstance(layer, tf.keras.layers.BatchNormalization):
+        beta, moving_mean, moving_variance = layer.get_weights()
+        betas.append(beta)
+        moving_means.append(moving_mean)
+        moving_variances.append(moving_variance)
+      
+def parse_bn(beta, moving_mean, moving_variance, num: int):
 
-    fc_weights[fc_weights == -1] = 0
-    fc_weights = fc_weights.T
-    xnor = ""
-    weight_dim = fc_weights.shape
-    for output_neuron in range(weight_dim[0]):
-        for input_neuron in range(weight_dim[1]):
-            weight = fc_weights[output_neuron][input_neuron]
-            if weight == 0:
-                xnor += f"assign xnor_result[{input_neuron}][{output_neuron}] = ~input_neuron[{input_neuron}];\n"
-            elif weight == 1:
-                xnor += f"assign xnor_result[{input_neuron}][{output_neuron}] = input_neuron[{input_neuron}];\n"
-            else:
-                raise Exception(f"neuron value not 0 or 1: {input_neuron}")
+    # thresholds = np.zeros(len(beta))
+    compare = ""
+    for output_neuron in range(len(beta)):
+        # print(len(beta))
+        threshold = moving_mean[output_neuron] - beta[output_neuron] * np.sqrt(moving_variance[output_neuron])
+        compare += f"   assign o_data[{output_neuron}] = i_data[{output_neuron}] > {threshold} ? 1 : 0;\n"
 
-    output_hdl = templates.FC_TEMPLATE \
-        .replace("%XNOR_GEN%", xnor) \
+    output_hdl = templates.BN_TEMPLATE \
+        .replace("%DIM_DATA%", str(len(beta))) \
         .replace("%LAYER_NUM%", str(num)) \
-        .replace("%CLASSIFIER%", str(classifier))
-    with open(f"gen_hdl/fc_layer_{num}.sv", "w") as f:
+        .replace("%COMPARE%", compare)
+        
+    with open(f"gen_hdl/bn_layer_{num}.v", "w") as f:
         f.write(output_hdl)
 
-def parse_fc(fc_weights, num: int, classifier: bool):
-    if classifier:
-        classifier = str(1)
-    else:
-        classifier = str(0)
-
-    fc_weights[fc_weights == -1] = 0
-    fc_weights = fc_weights.T
-    xnor = ""
-    weight_dim = fc_weights.shape
-    for output_neuron in range(weight_dim[0]):
-        for input_neuron in range(weight_dim[1]):
-            weight = fc_weights[output_neuron][input_neuron]
-            if weight == 0:
-                xnor += f"assign xnor_result[{input_neuron}][{output_neuron}] = ~input_neuron[{input_neuron}];\n"
-            elif weight == 1:
-                xnor += f"assign xnor_result[{input_neuron}][{output_neuron}] = input_neuron[{input_neuron}];\n"
-            else:
-                raise Exception(f"neuron value not 0 or 1: {input_neuron}")
-
-    output_hdl = templates.FC_TEMPLATE \
-        .replace("%XNOR_GEN%", xnor) \
-        .replace("%LAYER_NUM%", str(num)) \
-        .replace("%CLASSIFIER%", str(classifier))
-    with open(f"gen_hdl/fc_layer_{num}.sv", "w") as f:
-        f.write(output_hdl)
-
-
-# Extract weights
-with lq.context.quantized_scope(True):
-    weights = model.layers[1].get_weights()
-parse_fc(weights[0], 0)
+  
+for n in range(len(betas)):
+    parse_bn(betas[n], moving_means[n], moving_variances[n], n)
 
